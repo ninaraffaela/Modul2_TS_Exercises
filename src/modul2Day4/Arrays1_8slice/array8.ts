e8ba2615de1da5578e1379439bec3b4ae2d5addf
ninaraffaela <nina.r.ziegler@gmail.com>
2025-02-20 14:20:21 +0100

    day 4 excercises done

--- src/modul2Day4/Arrays1_8slice/array8.ts
+++ src/modul2Day4/Arrays1_8slice/array8.ts
@@ -1,2 +1,21 @@
+console.log(
+    "%c === slice the array ===", "background-color: teal;"
+);
 
 
+const destinations: string[] = [
+    "Schweden",
+    "Polen",
+    "Italien",
+    "Norwegen",
+    "Griechenland"
+]
+
+console.log(destinations);
+const removedAt2 = destinations.slice(2,4);
+console.log("removed", removedAt2);
+console.log(destinations);
+
+
+
+// - original array is not edited <3 i just temporarily take a piece out of it
